64c61f59e142ea81ac2c827428c6f76afff2ddd8
weixing <vincent.wei@backgardon.com>
2024-03-18 21:56:38 +0800

    update code

--- app/src/main/java/com/example/basicscodelab/contant/StaticData.kt
+++ app/src/main/java/com/example/basicscodelab/contant/StaticData.kt
@@ -4,18 +4,18 @@ import com.example.basicscodelab.R
 import com.example.basicscodelab.data.MoodBean
 
 object StaticData {
-    val moods = mutableListOf(
-        MoodBean(R.drawable.ic_happy, "Happy"),
-        MoodBean(R.drawable.ic_sweet, "Sweet"),
-        MoodBean(R.drawable.ic_warm, "Warm"),
-        MoodBean(R.drawable.ic_enrich, "Enrich"),
-        MoodBean(R.drawable.ic_smug, "Smug"),
-        MoodBean(R.drawable.ic_surprise, "Surprise"),
-        MoodBean(R.drawable.ic_calm, "Calm"),
-        MoodBean(R.drawable.ic_confused, "Confused"),
-        MoodBean(R.drawable.ic_grievance, "Grievance"),
-        MoodBean(R.drawable.ic_sad, "Sad"),
-        MoodBean(R.drawable.ic_irritability, "Irritability"),
-        MoodBean(R.drawable.ic_angry, "Angry"),
-    )
+  val moods = mutableListOf(
+    MoodBean(R.drawable.ic_happy, "Happy"),
+    MoodBean(R.drawable.ic_sweet, "Sweet"),
+    MoodBean(R.drawable.ic_warm, "Warm"),
+    MoodBean(R.drawable.ic_enrich, "Enrich"),
+    MoodBean(R.drawable.ic_smug, "Smug"),
+    MoodBean(R.drawable.ic_surprise, "Surprise"),
+    MoodBean(R.drawable.ic_calm, "Calm"),
+    MoodBean(R.drawable.ic_confused, "Confused"),
+    MoodBean(R.drawable.ic_grievance, "Grievance"),
+    MoodBean(R.drawable.ic_sad, "Sad"),
+    MoodBean(R.drawable.ic_irritability, "Irritability"),
+    MoodBean(R.drawable.ic_angry, "Angry"),
+  )
 }
